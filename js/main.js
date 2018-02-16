7941ff502545d6ad9bd7d8cc5a47cfa801472af2
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-17 02:01:20 +1100

    Creating pages for add and delete record

--- js/main.js
+++ js/main.js
@@ -1,63 +1,72 @@
 "use strict";
-const btnCustomers = document.querySelector("#customers");
-const btnAddACustomer = document.querySelector("#addACustomer");
-const btnClearCustomers = document.querySelector("#clearCustomer");
 
 // IndexedDB functionality
+let db;
 // Checks if the indexedDB is supported in the current browser
 if ("indexedDB" in window) {
-  // Creates a new database
-  const customerData = window.indexedDB.open("myDatabase", 1);
+  // Creates a new database 
+  const requestCustomer = window.indexedDB.open("myCustomers", 1);
 
-  customerData.onupgradeneeded = function(e) {
-    console.log("upgradeneede is running...");
-    let db = e.target.result;
-    // Creates object store named customers
-    let myObjectStore = db.createObjectStore("customers", {keyPath: "name"});
+  // Handles upgrade event
+  requestCustomer.onupgradeneeded = function(e) {
+    // Creates an object store with auto key generator
+    let thisDB = requestCustomer.result;
+    let customerObjectStore = thisDB.createObjectStore("customers1", { keyPath: "email" });
   }
 
-  // On success event
-  customerData.onsuccess = function(e) {
-    let db = e.target.result;
-    // Starts the new transaction
-    let myTransaction = db.transaction(["customers"], "readwrite");
-    // Reference to the object store we want to use
-    let store = myTransaction.objectStore("customers");
-    // Customers object
-    const myCustomers = [
-      { email: 'bob@bob.com', name: 'Bob' }, 
-      { email: 'dan@dan.com', name: 'Dan' },
-      { email: 'rich@rich.com', name: 'Rich' },
-      { email: 'pete@pete.com', name: 'Pete' }
-    ];
-    // Uses put method to add each customer to the object store
-    myCustomers.forEach((customer) => store.put(customer));
+  requestCustomer.onsuccess = function(e) {
+    db = requestCustomer.result;
+    console.log("success");
 
+
+
+    // Handles error event
     db.onerror = function(e) {
       let errorcode = e.target.errorCode;
-      console.log(`Error: ${errorcode}`);
-    };
-
-    // Query the data
-    let getDan = store.get("Dan");
-
-    getDan.onsuccess = function() {
-      console.log(getDan.result.email);
-    };
-
-    // Closes the db when the transaction is done
-    myTransaction.oncomplete = function() {
-      db.close();
-    };
+      console.log(`Error after success: ${errorcode}`);
+    }
   }
-
-  // On error and prints the error to the console
-  customerData.onerror = function(e) {
+  
+  // Handles error event
+  requestCustomer.onerror = function(e) {
     let errorcode = e.target.errorCode;
-    console.log(`There is an error: ${errorcode}`);
+    console.log(`Error: ${errorcode}`);
   }
 } else {
-  // Logs an error if indexedDB is not supported
+  // Logs an error message if indexedDB is not supported
   console.log("IndexedDB is not supported in this browser");
 }
 
+// Function to add a customer
+  function addCustomer(e) {
+    // Empty object to store new customer name and email values
+    let newCustomer = {};
+    const customerName = document.querySelector("#fullName").value;
+    const customerEmail = document.querySelector("#email").value;
+
+    // Adds new customer's name and email to the object
+    newCustomer.name = customerName;
+    newCustomer.email = customerEmail;
+console.log(db);
+    // Starts a new transaction
+    let customerTransaction = db.transaction(["customers1"], "readwrite");
+    // Reference to the object store we want to use
+    let customerStore = customerTransaction.objectStore("customers1");
+    let request = customerStore.add(newCustomer);
+
+    request.onsuccess = function(e) {
+      winodw.location.href = "index.html";
+      console.log(yes);
+    }
+
+    request.onerror = function(e) {
+      let errorcode = e.target.errorCode;
+      console.log(`Error: ${errorcode}`);
+    }
+  }
+
+  // Function to delete a customer
+  function deleteCustomer() {
+    const customerDeleteEmail = document.querySelector("#emailDelete").value;
+    console.log(customerDeleteEmail);
+  }
