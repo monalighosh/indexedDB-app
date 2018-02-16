49348f672599ae8659af31edd8b986324eed418d
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-16 21:55:05 +1100

    Creating indexedDB

--- js/main.js
+++ js/main.js
@@ -7,26 +7,54 @@ const btnClearCustomers = document.querySelector("#clearCustomer");
 // Checks if the indexedDB is supported in the current browser
 if ("indexedDB" in window) {
   // Creates a new database
-  const customerData = window.indexedDB.open("customer", 1);
+  const customerData = window.indexedDB.open("myDatabase", 1);
 
   customerData.onupgradeneeded = function(e) {
     console.log("upgradeneede is running...");
     let db = e.target.result;
-    if(!db.objectStoreNames.contains("firstOS")) {
-      db.createObjectStore("firstOS");
-    }
+    // Creates object store named customers
+    let myObjectStore = db.createObjectStore("customers", {keyPath: "name"});
   }
 
-  // On success prints the success message to the console
+  // On success event
   customerData.onsuccess = function(e) {
     let db = e.target.result;
-    console.log("Success!");
+    // Starts the new transaction
+    let myTransaction = db.transaction(["customers"], "readwrite");
+    // Reference to the object store we want to use
+    let store = myTransaction.objectStore("customers");
+    // Customers object
+    const myCustomers = [
+      { email: 'bob@bob.com', name: 'Bob' }, 
+      { email: 'dan@dan.com', name: 'Dan' },
+      { email: 'rich@rich.com', name: 'Rich' },
+      { email: 'pete@pete.com', name: 'Pete' }
+    ];
+    // Uses put method to add each customer to the object store
+    myCustomers.forEach((customer) => store.put(customer));
+
+    db.onerror = function(e) {
+      let errorcode = e.target.errorCode;
+      console.log(`Error: ${errorcode}`);
+    };
+
+    // Query the data
+    let getDan = store.getAll();
+
+    getDan.onsuccess = function() {
+      console.log(getDan.result);
+    };
+
+    // Closes the db when the transaction is done
+    myTransaction.oncomplete = function() {
+      db.close();
+    };
   }
 
   // On error and prints the error to the console
   customerData.onerror = function(e) {
     let errorcode = e.target.errorCode;
-    console.log(`Failed: ${errorcode}`);
+    console.log(`There is an error: ${errorcode}`);
   }
 } else {
   // Logs an error if indexedDB is not supported
