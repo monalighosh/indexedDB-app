ecabf7d82519fb90a2e52922c7667b6ed08aa3ab
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-16 16:24:54 +1100

    Updating wireframe

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const btnCustomers = document.querySelector("#customers");
+const btnAddACustomer = document.querySelector("#addACustomer");
+const btnClearCustomers = document.querySelector("#clearCustomer");
+
