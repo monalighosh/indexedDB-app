e39ff0d4c640b8c85f7df5e7794fda0c8742b12d
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-17 13:31:08 +1100

    Updtes in Add data functionality

--- js/main.js
+++ js/main.js
@@ -1,72 +1,71 @@
 "use strict";
-
-// IndexedDB functionality
-let db;
-// Checks if the indexedDB is supported in the current browser
-if ("indexedDB" in window) {
-  // Creates a new database 
-  const requestCustomer = window.indexedDB.open("myCustomers", 1);
-
-  // Handles upgrade event
+let database;
+// Check indexedDB is supported
+if("indexedDB" in window) {
+  // Create indexedDB database
+  let requestCustomer = window.indexedDB.open("customerDataNew", 1);
+  // Handle upgrade event
   requestCustomer.onupgradeneeded = function(e) {
-    // Creates an object store with auto key generator
-    let thisDB = requestCustomer.result;
-    let customerObjectStore = thisDB.createObjectStore("customers1", { keyPath: "email" });
+    let database = requestCustomer.result;
+    // Create object store
+    let customerObjStore = database.createObjectStore("customersD1", { autoIncrement: true, keyPath: "id" });
+    console.log(`upgrading.... ${customerObjStore}`);
   }
 
+  // Handle success event
   requestCustomer.onsuccess = function(e) {
-    db = requestCustomer.result;
-    console.log("success");
+    database = requestCustomer.result;
 
+    database.onerror = function(e) {
+      let errorCode = e.target.errorCode;
+      console.log(`Error within success handler: ${errorCode}`);
+    };
+    console.log("success....");
+  } 
 
-
-    // Handles error event
-    db.onerror = function(e) {
-      let errorcode = e.target.errorCode;
-      console.log(`Error after success: ${errorcode}`);
-    }
-  }
-  
-  // Handles error event
+  // Handle error event
   requestCustomer.onerror = function(e) {
-    let errorcode = e.target.errorCode;
-    console.log(`Error: ${errorcode}`);
+    let errorCode = e.target.errorCode;
+    console.log(`Error: ${errorCode}`);
   }
+
 } else {
-  // Logs an error message if indexedDB is not supported
-  console.log("IndexedDB is not supported in this browser");
+  // Throw an alert message if indexedDB is not supported
+  alert ("Sorry, IndexedDB is not supported in your browser!");
 }
 
-// Function to add a customer
-  function addCustomer(e) {
-    // Empty object to store new customer name and email values
-    let newCustomer = {};
-    const customerName = document.querySelector("#fullName").value;
-    const customerEmail = document.querySelector("#email").value;
-
-    // Adds new customer's name and email to the object
-    newCustomer.name = customerName;
-    newCustomer.email = customerEmail;
-console.log(db);
-    // Starts a new transaction
-    let customerTransaction = db.transaction(["customers1"], "readwrite");
-    // Reference to the object store we want to use
-    let customerStore = customerTransaction.objectStore("customers1");
-    let request = customerStore.add(newCustomer);
-
-    request.onsuccess = function(e) {
-      winodw.location.href = "index.html";
-      console.log(yes);
-    }
-
-    request.onerror = function(e) {
-      let errorcode = e.target.errorCode;
-      console.log(`Error: ${errorcode}`);
-    }
+// Function to add customers
+function addNewCustomer(e) {
+  // Prevent form submission
+  e.preventDefault();
+  // Store form input values
+  const name = document.querySelector("#fullName").value;
+  const email = document.querySelector("#email").value;
+  // Object to store new customer's details
+  const newCustomer = {
+    name,
+    email,
+    dateCreated: new Date()
   }
+  // Start transaction to add data
+  let requestTransaction = database.transaction(["customersD1"], "readwrite");
+  let objStore = requestTransaction.objectStore("customersD1");
+  let addRequest = objStore.put(newCustomer);
 
-  // Function to delete a customer
-  function deleteCustomer() {
-    const customerDeleteEmail = document.querySelector("#emailDelete").value;
-    console.log(customerDeleteEmail);
+  addRequest.onsuccess = function(e) {
+    console.log("Data is added");
+    window.location.assign("index.html");
+    
   }
+  
+  addRequest.onerror = function(e) {
+    console.log("Sorry, data is not added");
+  }
+  console.log(database);
+}
+
+// Function to delete customers
+function deleteCustomer(e) {
+  let customerEmail = document.querySelector("#emailToDelete").value;
+}
+
