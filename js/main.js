9fb035c2afa19d5d3227e1a9c47c47c196fe125d
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-17 16:17:03 +1100

    Updtes in displaying customer

--- js/main.js
+++ js/main.js
@@ -10,28 +10,30 @@ if("indexedDB" in window) {
     // Create object store
     let customerObjStore = database.createObjectStore("customersD1", { autoIncrement: true, keyPath: "id" });
     console.log(`upgrading.... ${customerObjStore}`);
-  }
+  };
 
   // Handle success event
   requestCustomer.onsuccess = function(e) {
     database = requestCustomer.result;
+    console.log("success....");
+    showCustomers();
+
 
     database.onerror = function(e) {
       let errorCode = e.target.errorCode;
       console.log(`Error within success handler: ${errorCode}`);
     };
-    console.log("success....");
-  } 
+  };
 
   // Handle error event
   requestCustomer.onerror = function(e) {
     let errorCode = e.target.errorCode;
     console.log(`Error: ${errorCode}`);
-  }
+  };
 
 } else {
   // Throw an alert message if indexedDB is not supported
-  alert ("Sorry, IndexedDB is not supported in your browser!");
+  alert("Sorry, IndexedDB is not supported in your browser!");
 }
 
 // Function to add customers
@@ -46,7 +48,7 @@ function addNewCustomer(e) {
     name,
     email,
     dateCreated: new Date()
-  }
+  };
   // Start transaction to add data
   let requestTransaction = database.transaction(["customersD1"], "readwrite");
   let objStore = requestTransaction.objectStore("customersD1");
@@ -55,13 +57,12 @@ function addNewCustomer(e) {
   addRequest.onsuccess = function(e) {
     console.log("Data is added");
     window.location.assign("index.html");
-    
-  }
+  };
   
   addRequest.onerror = function(e) {
-    console.log("Sorry, data is not added");
-  }
-  console.log(database);
+    let errorCode = e.target.errorCode;
+    console.log(`Sorry, data is not added. Error: ${errorCode}`);
+  };
 }
 
 // Function to delete customers
@@ -69,3 +70,21 @@ function deleteCustomer(e) {
   let customerEmail = document.querySelector("#emailToDelete").value;
 }
 
+// Function to show customers
+function showCustomers(e){
+// Start a transaction to get the data
+let getTransaction = database.transaction(["customersD1"], "readonly");
+let getStore = getTransaction.objectStore("customersD1");
+let getRequest = getStore.getAll();
+
+getRequest.onsuccess = function(e) {
+  let requestResult = getRequest.result;
+  console.log(`Got the data ${requestResult}`);
+};
+
+getRequest.onerror = function(e) {
+  let errorCode = e.target.errorCode;
+  console.log(`Error: ${errorCode}`);
+};
+
+}
